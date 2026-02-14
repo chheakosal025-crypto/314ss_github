b0e62d4cd19bfb2bcb4dd27044bf51d510a71304
chheakosal025-crypto <chheakosal025@gmail.com>
2026-02-14 10:38:28 +0700

    i love o

---
+++ ex04.py
@@ -0,0 +1,15 @@
+class vavi:
+  def __init__(self,bankaccound,blance,name,secret):
+        self.bankaccound=bankaccound
+        self.blance=blance
+        self.name=name
+        self.secret=secret
+  def show_info(self):
+    print(user1.bankaccound)
+    print(user1.name)
+    print(user1.blance)
+    print(user1.secret)
+user1 = vavi(bankaccound="kala", name="kola",blance="2000",secret="283838")
+user1.show_info()
+   
+
